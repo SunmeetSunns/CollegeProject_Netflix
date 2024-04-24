1e9ae99d6e01e620f122fbd8cd281c9986a9292d
SunmeetSunns <98201485+SunmeetSunns@users.noreply.github.com>
2024-04-24 17:57:38 +0530

    service phle jaisi

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 // Enable CORS middleware
@@ -11,24 +12,18 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const MongoDBCONNECT = 'mongodb+srv://kaursunmeet624:meet2002@cluster0.oozu3zq.mongodb.net/netflix?retryWrites=true&w=majority';
-
-mongoose.connect(MongoDBCONNECT, {
+mongoose.connect("mongodb+srv://kaursunmeet624:meet2002@cluster0.oozu3zq.mongodb.net/netflix", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
+    useUnifiedTopology: true,
+}).then(() => {
     console.log('DB Connected');
-})
-.catch(err => {
-    console.error('Error connecting to MongoDB:', err);
 });
 
 // User routes
 app.use('/api/user', userRoutes);
 
 // Start server
-const PORT = process.env.PORT ||5000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
